847aed20f838c7b572e0d4723c8dcdfebe9c6438
Ikechukwu-Ik <133080510+Ikechukwu-Ik@users.noreply.github.com>
2025-02-10 12:08:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const toggleButton = document.getElementById("toggle-button");
 const menuList = document.getElementById("menu-list");
+const contactForm = document.getElementById("contact-form");
+
+
 
 toggleButton.addEventListener("click", function showMenu() {
 	menuList.classList.toggle("active");
@@ -14,7 +17,20 @@ document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click",
 
 
 
+contactForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    var username = document.getElementById('username').value.trim();
+		var address = document.getElementById('address').value.trim();
+		var message = document.querySelector('.message-area').value.trim();
+
+    confirm ("Do you want to send this message to MU Shopping Mall?\n\n" + "From: " + username + "\nAddress: " + address + "\nContent: " + message);
+});
+
+
+
+
 ////////////////////////////////////////////////////
 function showPopup() {
-	confirm ("Do you want to send this message?.",);
+	alert ("Message sent successfully.",);
 }
